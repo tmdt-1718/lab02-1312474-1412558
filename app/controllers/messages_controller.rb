b754c12dd5e16b7f4a7799a9f27c3afb7f498042
Phạm Quốc Toàn <bossdiemmaimai@gmail.com>
2017-10-13 17:36:24 +0700

    update

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,6 @@
 class MessagesController < ApplicationController
   def index
   end
+  def show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'messages/index'
+
+
 get 'messages/inbox', to: 'messages#inbox'
+get 'messages/sent', to: 'messages#sent'
+get 'messages/show', to: 'messages#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
