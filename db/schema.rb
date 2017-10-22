00239e18d3a1ca31f6e6d87cbbe04e44c87987b6
1312474 <1312474@student.hcmus.edu.vn>
2017-10-22 21:40:57 +0700

    fix add fr

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171017061502) do
+ActiveRecord::Schema.define(version: 20171022091532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 20171017061502) do
     t.text "body"
     t.boolean "read", default: false
     t.datetime "readed_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tablefriends", force: :cascade do |t|
+    t.integer "id_acc"
+    t.integer "id_friend"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
