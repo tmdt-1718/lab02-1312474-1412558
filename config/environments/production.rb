f78b2c6c11698f9cb06fb543970cec266682c9ad
Phạm Quốc Toàn <bossdiemmaimai@gmail.com>
2017-10-23 14:32:16 +0700

    config/environments/production.rb mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,66 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+
+    config.action_mailer.smtp_settings = {
+        :address => 'smtp.gmail.com',
+        :port => 587,
+        :domain => 'gmail.com',
+        :authentication => 'plain',
+        :user_name => 'ec.app.mail@gmail.com',
+        :password => '@ECappmail',
+        :enable_starttls_auto => true
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   # Code is not reloaded between requests.
   config.cache_classes = true
