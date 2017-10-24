4c7ea548dc5096e26c786ba1b0f9bfa3cae45ad3
1312474 <1312474@student.hcmus.edu.vn>
2017-10-24 11:10:44 +0700

    them layout login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # default
-    devise_for :users
+    devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
     root 'messages#inbox'
     get 'home', to: 'home#index'
     get 'friends', to: 'friends#index'
